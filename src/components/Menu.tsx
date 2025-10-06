ebade0efb5b8dddd408b6f476b09c4e7edf0e104
SAGARA DILSHAN <sagaradilshan369@gmail.com>
2025-10-06 15:14:38 +0000

    items doesnt load?

--- src/components/Menu.tsx
+++ src/components/Menu.tsx
@@ -38,7 +38,7 @@ export default function Menu() {
   }
 
   return (
-    <div className="grid grid-cols-1 sm-grid-cols-2 xl:grid-cols-3 gap-6">
+    <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 gap-6">
       {menuItems?.map((item) => (
         <MenuItemCard key={item.id} item={item} />
       ))}
