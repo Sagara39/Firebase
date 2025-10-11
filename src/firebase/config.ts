e6d2d28a93a300f6f4afedf479203f8d612af263
SAGARA DILSHAN <sagaradilshan369@gmail.com>
2025-10-11 14:43:42 +0000

    Set up a Firebase backend

---
+++ src/firebase/config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+  "projectId": "studio-5297353122-b4093",
+  "appId": "1:1023794794433:web:47f37b59fe724e6d002940",
+  "apiKey": "AIzaSyC1jeU2f7DkDDe41En9BTqovUy2KdfZxjg",
+  "authDomain": "studio-5297353122-b4093.firebaseapp.com",
+  "measurementId": "",
+  "messagingSenderId": "1023794794433"
+};
